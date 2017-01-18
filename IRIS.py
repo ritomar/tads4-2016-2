538261465a6e1de75c6540520ca58561057a6ca3
ritomar <ritomar@hotmail.com>
2017-01-18 16:53:02 -0300

    Aula em 20170118

---
+++ IRIS.py
@@ -0,0 +1,23 @@
+def FazerBolo():
+    print("Separar os ingredientes")
+    print("Misturar os ingredientes em um recipiente")
+    print("Deixar no forno por 30 minutos")
+    print("Retirar do forno")
+    print("")
+
+def AtravesarRua():
+    print("Parar na borda da calçada")
+    print("Olhar para direita")
+    print("Olhar para esquerda")
+    print("Senão tiver trânsito, atravesar")
+    print("")
+
+def Saudacao():
+    nome = input("Diga seu nome: ")
+    print("Olá," + nome)
+    print("")
+
+FazerBolo()
+AtravesarRua()
+AtravesarRua()
+Saudacao()
